11c088e75f2e0be9dd1851e3210497ac18a718d5
Shivang Panchal <panchalshivang938@gmail.com>
2025-04-04 22:44:57 +0530

    Add files via upload

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Header from './components/header/header'
+import Home from './components/Home/Home';
+
+function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App;
---
+++ reactRouter/src/main.jsx
@@ -0,0 +1,42 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
+import Layout from './Layout';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import User from './components/User/User';
+import Github, { githubInfoLoader } from './components/Github/Github';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />  
+      <Route path="about" element={<About />} /> 
+      <Route path="contact" element={<Contact />} />  
+      <Route path="user/:userid" element={<User />} />
+      <Route
+      loader={githubInfoLoader}
+       path="github"
+        element={<Github />}
+        />
+      </Route>
+  )
+);
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<Layout />}>
+//       <Route index element={<Home />} />  {/* ✅ Use 'index' for home */}
+//       <Route path="about" element={<About />} />  {/* ✅ No leading '/' */}
+//       <Route path="contact" element={<Contact />} />  {/* ✅ No leading '/' */}
+//     </Route>
+//   )
+// );
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
